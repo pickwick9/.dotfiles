9056e00e71793559ba8e6a451f53f7b9532f370e
pickwick9 <aca9dnp@protonmail.com>
2023-10-05 21:07:35 -0400

    small changes to indentations in packer.lua

--- nvim/.config/nvim/lua/config/packer.lua
+++ nvim/.config/nvim/lua/config/packer.lua
@@ -3,43 +3,43 @@
 vim.cmd.packadd('packer.nvim')
 
 require('packer').startup(function()
-  use 'wbthomason/packer.nvim' -- packer can manage itself
+    use 'wbthomason/packer.nvim' -- packer can manage itself
 
-  use({"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"})
-  use("theprimeagen/harpoon")
-  use("theprimeagen/vim-be-good")
-  use("mbbill/undotree")
-  use("tpope/vim-fugitive")
-  use("tpope/vim-commentary")
-  use({ "rose-pine/neovim", as = "rose-pine" })
+    use({"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"})
+    use("theprimeagen/harpoon")
+    use("theprimeagen/vim-be-good")
+    use("mbbill/undotree")
+    use("tpope/vim-fugitive")
+    use("tpope/vim-commentary")
+    use({ "rose-pine/neovim", as = "rose-pine" })
 
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = {{'nvim-lua/plenary.nvim'}}
-  }
+    use {
+        'nvim-telescope/telescope.nvim',
+        requires = {{'nvim-lua/plenary.nvim'}}
+    }
 
-  use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v1.x',
-	  requires = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},
-		  {'williamboman/mason.nvim'},
-		  {'williamboman/mason-lspconfig.nvim'},
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
 
-		  -- Autocompletion
-		  {'hrsh7th/nvim-cmp'},
-		  {'hrsh7th/cmp-buffer'},
-		  {'hrsh7th/cmp-path'},
-		  {'saadparwaiz1/cmp_luasnip'},
-		  {'hrsh7th/cmp-nvim-lsp'},
-		  {'hrsh7th/cmp-nvim-lua'},
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-buffer'},
+            {'hrsh7th/cmp-path'},
+            {'saadparwaiz1/cmp_luasnip'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'hrsh7th/cmp-nvim-lua'},
 
-		  -- Snippets
-		  {'L3MON4D3/LuaSnip'},
-		  {'rafamadriz/friendly-snippets'},
-	  }
-  }
+            -- Snippets
+            {'L3MON4D3/LuaSnip'},
+            {'rafamadriz/friendly-snippets'},
+        }
+    }
 
 end)
 
