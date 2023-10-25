16b872bfe98e956175b19a640b6a92f38cf42a3f
pickwick9 <aca9dnp@protonmail.com>
2023-10-25 01:57:59 -0400

    update

--- nvim/.config/nvim/after/plugin/debug.lua
+++ nvim/.config/nvim/after/plugin/debug.lua
@@ -18,20 +18,20 @@ vim.keymap.set("n", "<leader>dj", ":DapStepOver<CR>")
 local dap = require('dap')
 
 dap.adapters.gdb = {
-  type = "executable",
-  command = "gdb",
-  args = { "-i", "dap" }
+    type = "executable",
+    command = "gdb",
+    args = { "-i", "dap" }
 }
 
 dap.configurations.rust = {
-  {
-    name = "Launch",
-    type = "gdb",
-    request = "launch",
-    program = function()
-      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-    end,
-    cwd = "${workspaceFolder}",
-  },
+    {
+        name = "Launch",
+        type = "gdb",
+        request = "launch",
+        program = function()
+          return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+        cwd = "${workspaceFolder}",
+    },
 }
 
