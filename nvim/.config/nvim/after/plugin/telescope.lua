6937db1932d9ac8ef9321b6eecad41530bdacd01
pickwick9 <aca9dnp@protonmail.com>
2023-10-25 02:07:06 -0400

    update

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,9 +1,4 @@
 local builtin = require('telescope.builtin')
 
+vim.keymap.set('n', '<leader>f', function() builtin.find_files({ hidden = true }) end, {})
 vim.keymap.set('n', '<leader>b', builtin.buffers, {})
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
-vim.keymap.set('n', '<leader>fs', function()
-      builtin.grep_string({ search = vim.fn.input("Grep > ") } );
-end)
-
